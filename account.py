a1e1b5dfcde24e343e874f22a6c43a7063758b48
czagurski <131494048+czagurski@users.noreply.github.com>
2023-04-29 20:00:34 -0500

    Readded comments on the test branch, copied from other file into the new repository

--- account.py
+++ account.py
@@ -1,9 +1,17 @@
 class Account:
     def __init__(self, name):
+        """
+        Creates new account class instance
+        """
         self.account_name = name
         self.account_balance = 0
 
     def deposit(self, amount):
+        """"
+        Deposits given amount into account
+        :param amount: The amount given to deposit
+        :return: True of the deposit was successful, False otherwise.
+        """
         if amount > 0:
             self.account_balance += amount
             return True
@@ -11,6 +19,11 @@ class Account:
             return False
 
     def withdraw(self, amount):
+        """
+        Takes out amount of money given from the account
+        :param amount: The amount of money to withdraw.
+        :return: True if the withdrawal was successful, False otherwise.
+        """
         if (amount > 0) and (amount <= self.account_balance):
             self.account_balance -= amount
             return True
@@ -18,7 +31,15 @@ class Account:
             return False
 
     def get_balance(self):
+        """
+        Returns current balance
+        :return: The current balance of the account.
+        """
         return self.account_balance
 
     def get_name(self):
+        """
+        Returns name of the account owner
+        :return: The name of the account owner.
+        """
         return self.account_name
